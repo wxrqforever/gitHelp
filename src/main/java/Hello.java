eb25aa3405eda7911f6a3e72c2f48f96977cb6b0
wx <wx@123.com>
2022-07-04 10:36:24 +0800

    idea git help from hotfix

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git form hotfix");
     }
 }
